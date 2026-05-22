625091d3fcdb164b833b2059ade152e8b24ae93c
devil233 <69190444+devil233-ui@users.noreply.github.com>
2026-05-23 00:45:57 +0800

    增加对 matcher.plugin 为 None 的兜底容错

--- nonebot_plugin_authrespond/run.py
+++ nonebot_plugin_authrespond/run.py
@@ -7,7 +7,6 @@ from nonebot_plugin_session import EventSession
 
 superusers = get_driver().config.superusers
 
-
 @run_preprocessor
 async def pass_run(
     matcher: Matcher,
@@ -27,10 +26,14 @@ async def pass_run(
 
 
 async def pass_rule(user_id: str, matcher: Matcher):
+    if matcher.plugin:
+        modulename = matcher.plugin.name
+    else:
+        modulename = matcher.plugin_name or "dynamic"
 
-    modulename = matcher.plugin.name
     if modulename.startswith("nonebot_plugin_"):
         modulename = modulename.replace("nonebot_plugin_", "")
+        
     msg = f"plugin {modulename} is responses by {user_id}, check permission!"
     logger.opt(colors=True).debug(msg)
     check , mode = cubp.checkperm(modulename, user_id)
@@ -44,6 +47,24 @@ async def pass_rule(user_id: str, matcher: Matcher):
 
 
 async def passgroup_rule(user_id: str, group_id: str, matcher: Matcher):
+    if matcher.plugin:
+        modulename = matcher.plugin.name
+    else:
+        modulename = matcher.plugin_name or "dynamic"
+        
+    if modulename.startswith("nonebot_plugin_"):
+        modulename = modulename.replace("nonebot_plugin_", "")
+        
+    msg = f"plugin {modulename} is responses by group {group_id}, check permission!"
+    logger.opt(colors=True).debug(msg)
+    check , mode = cubp.checkpermgroup(modulename, group_id)
+    if check:
+        msg = f"ID {group_id} group is not allow run {modulename}"
+        logger.opt(colors=True).warning(msg)
+        if user_id in superusers:
+            return "white"
+        raise IgnoredException(msg) from None
+    return mode
     modulename = matcher.plugin.name
     if modulename.startswith("nonebot_plugin_"):
         modulename = modulename.replace("nonebot_plugin_", "")
